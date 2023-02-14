d663f189f5e1f33ff7893f08cf7626e28b256d78
Melvin Vasquez <mvasq95@hotmail.com>
2023-02-14 11:45:14 -0500

    pseudocoding and notes added to the navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,18 +2,29 @@ import React, { useEffect } from 'react';
 
 const Navbar = props => {
 
-    useEffect(() => {
-        // Get the navbar-toggler button element
-        const navbarToggler = document.querySelector('.navbar-toggler');
-        // Get the menu items container element
-        const menuContainer = document.querySelector('.navbar-collapse');
+    //state logic
+    //at route '/'
+        //if user is not signed in
+            //Sign-up button only in nav - will be modal
+                //login form will be a part of the homepage
+        //if user is logged in
+            //show nav links and logout button
+            //on mobile view, show collapsible menu.
+        
 
-        // Attach a click event listener to the navbar-toggler button
-        navbarToggler.addEventListener('click', () => {
-            // Toggle the "show" class on the menu items container
-            menuContainer.classList.toggle('collapse');
-        });
-    }, []);
+    //comment out useEffect until you know how to struct navbar buttons/links
+    // useEffect(() => {
+    //     // Get the navbar-toggler button element
+    //     const navbarToggler = document.querySelector('.navbar-toggler');
+    //     // Get the menu items container element
+    //     const menuContainer = document.querySelector('.navbar-collapse');
+
+    //     // Attach a click event listener to the navbar-toggler button
+    //     navbarToggler.addEventListener('click', () => {
+    //         // Toggle the "show" class on the menu items container
+    //         menuContainer.classList.toggle('collapse');
+    //     });
+    // }, []);
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -33,6 +44,21 @@ const Navbar = props => {
                         </li>
                     </ul>
                 </div>
+
+                {/* Commenting out nav-buttons until I figure out more the logic for it to render */}
+
+                {/* <div className="nav-buttons">
+                    <button className="btn btn-light">
+                        Login
+                    </button>
+                    <button className="btn btn-primary">
+                        Sign up
+                    </button>
+                    <button className="btn btn-danger">
+                        Log out
+                    </button>
+                </div> */}
+
             </div>
         </nav>
     )
