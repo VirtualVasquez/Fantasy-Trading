8181769179427ccb6439640f79ec147aa753595d
Melvin Vasquez <mvasq95@hotmail.com>
2023-03-01 02:26:15 -0500

    refactored. Work previously done from TradePage meant to be labeled as HomePage

--- client/src/modules/HomePage/index.js
+++ client/src/modules/HomePage/index.js
@@ -1,10 +1,20 @@
 import React, {useEffect, useState } from 'react';
-// import axios from 'axios';
+import './HomePage.scss';
+import BalanceSummary from './BalanceSummary.js';
+import HoldingsTable from './HoldingsTable.js';
 
 const HomePage = props => {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className='container trade-page'>
+            <div className='row'>
+                <div className='col-12'>
+                    <BalanceSummary />
+                </div>
+                <div className='col-12'>
+                    <HoldingsTable />
+                </div>     
+            </div>
+
         </div>
     )
 }
--- client/src/modules/TradePage/index.js
+++ client/src/modules/TradePage/index.js
@@ -1,20 +1,9 @@
 import React, {useEffect, useState } from 'react';
-import './tradePage.scss';
-import BalanceSummary from './BalanceSummary.js';
-import HoldingsTable from './HoldingsTable.js';
 
 const TradePage = props => {
     return (
-        <div className='container trade-page'>
-            <div className='row'>
-                <div className='col-12'>
-                    <BalanceSummary />
-                </div>
-                <div className='col-12'>
-                    <HoldingsTable />
-                </div>     
-            </div>
-
+        <div>
+            <h1>TradePage</h1>
         </div>
     )
 }
