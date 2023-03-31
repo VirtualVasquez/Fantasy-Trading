626af6c4a7a06931b7a01048e4068f233626f6ab
Melvin Vasquez <mvasq95@hotmail.com>
2023-03-31 03:45:43 -0400

    added login functionality. Need to refactor to use jwt token work, then connect to frontend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt');
 
 const jwt = require('jsonwebtoken');
 
@@ -18,10 +19,44 @@ const posts = [
     }
 ]
 
+const users = []
+
 
 app.get('/posts', authenticateToken, (req, res) => {
     res.json(posts.filter(post => post.username === req.user.name))
     res.json(posts);
+})
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { name: req.body.name, password: hashedPassword }
+        users.push(user)
+        res.status(201).send();
+    } catch { 
+        res.status(500).send();
+    }
+
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.anme === req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+            res.send('Success')
+        } else {
+            res.send('Not allowed');
+        }
+    } catch {
+        res.status(500).send();
+    }
 })
 
 
