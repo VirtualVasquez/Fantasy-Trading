d76100c20cef246f424c3da3b94f426ada9fa786
CITADEL\mvasq <mvasq95@hotmail.com>
2023-04-15 17:05:03 -0400

    starting to setup login logic in the front end.

--- client/src/modules/LoginPage/LoginForm.js
+++ client/src/modules/LoginPage/LoginForm.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
+async function loginUser(email, password){
+    try {
+        const response = await axios.post('users/login', {
+            email: email,
+            password: password
+        });
+
+        //somewhere in the response should be an accesstoken we can store. 
+        localStorage.setItem('fantasy_access_token', response.data.accessToken) // MAY NEED TO CHANGE
+        //redirect might need to happen at the app level, but should occur once accessToken is achieved and stored in local storage.
+        window.location.replace('/chat');
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 const LoginForm = props => {
+    const [providedEmail, setEmail] = useState(null);
+    const [providedPassword, setPassword] = useState(null);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        loginUser(email, password);
+    }
+
+
     return(
         <form id="login-form" className="account-form">
             <div class="form-group email-group">
