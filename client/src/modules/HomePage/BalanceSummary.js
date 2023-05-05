19f2849a1a2c0b1a7bd87722ab681ac31744c09d
CITADEL\mvasq <mvasq95@hotmail.com>
2023-05-04 21:53:29 -0400

    min. wireframe for account summary table

--- client/src/modules/HomePage/BalanceSummary.js
+++ client/src/modules/HomePage/BalanceSummary.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 const BalanceSummary = props => {
     return(
-        <div className="balance-summary">
-            <h2>My Balance</h2>
-            <p>Content Here</p>
+        <div className="balance-table">
+            <h2>Account Summary</h2>
+            <table className="table table-striped table">
+                <thead>
+                    <tr>
+                        <th scope="col">Account Value</th>
+                        <th scope="col">Cash Balance</th>
+                        <th scope="col">Market Value</th>
+                        <th scope="col">Base Cost</th>
+                        <th scope="col">Gain/Loss</th>
+                    </tr>            
+                </thead>
+                <tbody >
+                    <td className="account-value">
+                        $100,000
+                    </td>
+                    <td className="cash-balance">
+                        $100,000
+                    </td>
+                    <td className="market-value">
+                        $0
+                    </td>
+                    <td className="base-cost">
+                        $0
+                    </td>
+                    <td className="gain-loss">
+                        +$0.00(+0.0%)
+                    </td>
+
+                </tbody>
+            </table>
         </div>
     )
     
--- client/src/modules/HomePage/index.js
+++ client/src/modules/HomePage/index.js
@@ -6,6 +6,11 @@ import BalanceSummary from './BalanceSummary.js';
 import HoldingsTable from './HoldingsTable/index.js';
 
 const HomePage = props => {
+    //axios request for user's transactions
+        //make account summary end point
+        //make post request
+        //only market value needs to query finnhub for current price of all user's stocks
+
     
     return (
         <div className='container home-page'>
