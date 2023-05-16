3620eefa8c5f9cc06366edf8bf32941003eb7b84
CITADEL\mvasq <mvasq95@hotmail.com>
2023-05-16 14:31:42 -0400

    starting work needed for modal. Moved functions and hooks out of homepage to app.js to reuse in tradepage

--- client/src/modules/common/Modal/index.js
+++ client/src/modules/common/Modal/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './Modal.scss';
 
-const Modal = props => {
+function Modal({modalContents, toggleModal}) {
+    
+    const {symbol, sharePrice, availableFunds, currentOwnedShares} = modalContents;
+
     return(
     <div className="modal-div" id="modal">
         <div className="content">
             <h2>Modal Window</h2>
+            <p>{symbol}</p>
+            <p>{sharePrice}</p>
+            <p>{availableFunds}</p>
+            <p>{currentOwnedShares}</p>
             <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis deserunt corrupti, ut fugit magni qui quasi nisi amet repellendus non fuga omnis a sed impedit explicabo accusantium nihil doloremque consequuntur.
             </p>
             <div className="actions">
                 <button 
                     className="toggle-button"
-                    onClick={props.toggleModal}
+                    onClick={toggleModal}
                 >
                     OK
                 </button>
