58b8919228005941971caab0f26043cd37e72f20
CITADEL\mvasq <mvasq95@hotmail.com>
2023-05-31 12:21:50 -0400

    removing hardcoding of localhost:3001

--- client/src/App.js
+++ client/src/App.js
@@ -86,7 +86,7 @@ function App() {
   }
   async function getTransactions(token){
     try {
-        const response = await axios.get('http://localhost:3001/transactions', {
+        const response = await axios.get('/transactions', {
             params: {
                 accessToken: token
             }
