2e41087c839e72c40baff0f7462e84096d486332
VirtualVasquez <mvasq95@hotmail.com>
2024-01-02 18:01:44 -0500

    localhost:3001 is correct url (for some reason). changed .env variable of DB_HOST from localhost to db, to match name of database service in yaml file. Need to understand how this works, plus reconfigure to make usable on local environments

--- client/src/helpers/axiosConfig.js
+++ client/src/helpers/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL || 'http://localhost:3001',
+    baseURL:  'http://localhost:3001'
 });
 
 export default instance;
