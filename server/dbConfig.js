30f73edd3ba3406f42b32dfcf9a5f069f77d478a
VirtualVasquez <mvasq95@hotmail.com>
2024-01-02 20:13:48 -0500

    resolved docker vs local running issue

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,6 +1,9 @@
 require('dotenv').config({ path: '../.env' });
 const { Pool } = require('pg');
 
+//if running in Docker, DB_HOST in .env should equal 'db'
+//if running in local env, DB_HOST in .env should equal 'localhost'
+
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
